5f98d4d2a76f29a052b5ceb04c4ad22bb15beb5a
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-09 13:43:42 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
+variable "api_key" {
+type = string
+description = "API Key Id from Intersight"
+}
+variable "secretkey" {
+type = string
+description = "The path to your secretkey file for Intersight"
+}
+variable "api_endpoint" {
+/*type = string
+description = "The Intersight end point"*/
+default = "https://www.intersight.com"
+}
+variable "cluster_name" {
+type = string
+}
+
 #__________________________________________________________
 #
 # Terraform Cloud Organization
