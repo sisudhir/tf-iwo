3c664fc777a05e3f7282593a624b23245f582d61
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-09 13:42:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    intersight = {
+      source = "CiscoDevNet/intersight"
+      version = ">=1.0.11"
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.3.0"
+    }    
+  }
+}
+
 provider "intersight" {
   apikey    = var.api_key
   secretkey = var.secretkey
