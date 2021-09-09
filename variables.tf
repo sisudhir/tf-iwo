0532e5100308a3c0be8bce8d3f50f54a68341243
Sindhu Sudhir <sisudhir@cisco.com>
2021-09-09 16:40:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,6 +6,11 @@ variable "secretkey" {
   type = string
   description = "The path to your secretkey file for Intersight"
 }
+variable "api_endpoint" {
+/*type = string
+description = "The Intersight end point"*/
+default = "https://www.intersight.com"
+}
 variable "cluster_name" {
   type = string
 }
